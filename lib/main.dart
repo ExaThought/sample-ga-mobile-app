a150b65bf34e27e1d64fd275a8fad51ce50d34ce
VaishakMB08 <vaishak.mb@exathought.com>
2023-05-09 16:54:10 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'firebase_options.dart';
 import 'src/web_view_stack.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // await Firebase.initializeApp();
   runApp(
     MaterialApp(
       theme: ThemeData(useMaterial3: true),
@@ -20,13 +28,24 @@ class WebViewApp extends StatefulWidget {
 
 class _WebViewAppState extends State<WebViewApp> {
   late final WebViewController controller;
+  FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+
+//   firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+// firebaseAnalytics.setMeasurementEnabled(true, "<GA4_MEASUREMENT_ID>");
 
   @override
   void initState() {
     super.initState();
+    firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+    // firebaseAnalytics.setMeasurementEnabled(true, "G-H382LDYLBZ");
+    firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+    firebaseAnalytics.setDefaultEventParameters(
+        <String, dynamic>{'stream_id': '5140560412'});
+    firebaseAnalytics.setUserProperty(name: 'app-platform', value: 'app');
+
     controller = WebViewController()
       ..loadRequest(
-        Uri.parse('http://ga-nextjs-web.s3-website.ap-south-1.amazonaws.com/'),
+        Uri.parse('http://ga-nextjs-web.s3-website.ap-south-1.amazonaws.com?fromMobile=1'),
       );
   }
 
@@ -38,6 +57,16 @@ class _WebViewAppState extends State<WebViewApp> {
         body: WebViewStack(controller: controller),
       ),
       onWillPop: () async {
+        // print("*********");
+        // await FirebaseAnalytics.instance.logLogin();
+        FirebaseAnalytics.instance.logEvent(
+          name: "TestExathought",
+          parameters: {
+            // "mobile users": ,
+            "test": 1234,
+            // EventParamConstant.other_information: event.otherInformation
+          },
+          );
         if (await controller.canGoBack()) {
           await controller.goBack();
         } else {
