3359869b1ad9207eb289059de3c5950bba5288e6
jeevitha-jr <jeevitha.jr@exathought.com>
2023-05-12 13:35:40 +0530

    test commenting

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:newrelic_mobile/newrelic_mobile.dart';
+// import 'package:newrelic_mobile/newrelic_mobile.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 // import 'src/menu.dart';
 import 'src/web_view_stack.dart';
-import 'package:newrelic_mobile/config.dart';
+// import 'package:newrelic_mobile/config.dart';
 void main() {
   var appToken = "AA00e78b5d4cff2b845e596589507d0795a9bed32f-NRMA";
 
@@ -16,50 +16,51 @@ void main() {
   //     appToken = "<ios app token>"; // Replace with your application token copied from the New Relic UI.
   // }
 
-  Config config = Config(
-      accessToken: appToken,
+  // Config config = Config(
+  //     accessToken: appToken,
 
-      //Android Specific
-      // Optional: Enable or disable collection of event data.
-      analyticsEventEnabled: true,
+  //     //Android Specific
+  //     // Optional: Enable or disable collection of event data.
+  //     analyticsEventEnabled: true,
 
-      // Optional: Enable or disable reporting successful HTTP requests to the MobileRequest event type.
-      networkErrorRequestEnabled: true,
+  //     // Optional: Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+  //     networkErrorRequestEnabled: true,
 
-      // Optional: Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
-      networkRequestEnabled: true,
+  //     // Optional: Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+  //     networkRequestEnabled: true,
 
-      // Optional: Enable or disable crash reporting.
-      crashReportingEnabled: true,
+  //     // Optional: Enable or disable crash reporting.
+  //     crashReportingEnabled: true,
 
-      // Optional: Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
-      interactionTracingEnabled: true,
+  //     // Optional: Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+  //     interactionTracingEnabled: true,
 
-      // Optional: Enable or disable capture of HTTP response bodies for HTTP error traces and MobileRequestError events.
-      httpResponseBodyCaptureEnabled: true,
+  //     // Optional: Enable or disable capture of HTTP response bodies for HTTP error traces and MobileRequestError events.
+  //     httpResponseBodyCaptureEnabled: true,
 
-      // Optional: Enable or disable agent logging.
-      loggingEnabled: true,
+  //     // Optional: Enable or disable agent logging.
+  //     loggingEnabled: true,
 
-      // iOS specific
-      // Optional: Enable or disable automatic instrumentation of WebViews
-      webViewInstrumentation: true,
+  //     // iOS specific
+  //     // Optional: Enable or disable automatic instrumentation of WebViews
+  //     webViewInstrumentation: true,
 
-      //Optional: Enable or disable Print Statements as Analytics Events
-      printStatementAsEventsEnabled: true,
+  //     //Optional: Enable or disable Print Statements as Analytics Events
+  //     printStatementAsEventsEnabled: true,
 
-      // Optional: Enable or disable automatic instrumentation of HTTP Request
-      httpInstrumentationEnabled: true);
+  //     // Optional: Enable or disable automatic instrumentation of HTTP Request
+  //     httpInstrumentationEnabled: true);
 
-  NewrelicMobile.instance.start(config, () {
+  // NewrelicMobile.instance.start(config, () {
     runApp(
       MaterialApp(
         theme: ThemeData(useMaterial3: true),
         home: const WebViewApp(),
       ),
     );
-  });
-}
+  }
+  // );
+// }
 
 class WebViewApp extends StatefulWidget {
   const WebViewApp({super.key});
@@ -73,9 +74,9 @@ class _WebViewAppState extends State<WebViewApp> {
 
   @override
   void initState() {
-    NewrelicMobile.instance.setUserId("test-id");
-    NewrelicMobile.instance.recordCustomEvent("newrelic mobile iuser",
-        eventAttributes: {"user type": "mobile"}, eventName: "mobile newrelicuser");
+    // NewrelicMobile.instance.setUserId("test-id");
+    // NewrelicMobile.instance.recordCustomEvent("newrelic mobile iuser",
+    //     eventAttributes: {"user type": "mobile"}, eventName: "mobile newrelicuser");
 
     super.initState();
     controller = WebViewController()
